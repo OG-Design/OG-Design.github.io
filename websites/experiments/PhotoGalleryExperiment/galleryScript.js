e7275909bd48cb7d6347f15b4d14baae4e3aa734
peremarai <marcus@olsen-graphics.no>
2024-11-07 19:09:20 +0100

    mehhh js

--- websites/experiments/PhotoGalleryExperiment/galleryScript.js
+++ websites/experiments/PhotoGalleryExperiment/galleryScript.js
@@ -2,21 +2,35 @@ let imageArray = [];
 
 
 
-let newImage = new Image();
+function newImage(src, width, height, alt) {
+    
+    // creates image element
+    let img = document.createElement("img");
+    
+
+    // properties
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    img.alt = alt;
+
+    document.querySelectorAll("imageGalleryElement").appendChild="img";
+
+}
 
 
 // img list
-newImage.src = '/img/Gallery/3D/1001.png';
-newImage.src = '/img/Gallery/3D/IMG_0525.jpg';
-newImage.src = '/img/Gallery/3D/IMG_0526.jpg';
-newImage.src = '/img/Gallery/3D/IMG_0528.jpg';
+newImage(
+    "/img/Gallery/3D/1001.png", 500, 500, "alt"
+);
+// newImage.src = '/img/Gallery/3D/IMG_0525.jpg';
+// newImage.src = '/img/Gallery/3D/IMG_0526.jpg';
+// newImage.src = '/img/Gallery/3D/IMG_0528.jpg';
 // newImage.src('/img/Gallery/3D/ ');
 
 
 
-// img pushing
-imageArray.push(newImage);
 
-let displayArray = document.querySelector('imageGalleryElement').innerHTML;
 
-displayArray;
+
+
