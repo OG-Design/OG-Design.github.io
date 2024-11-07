107c5577ec1a6dcfec5b2d223aa83c5f697340ec
OG-Design <marcus@olsen-graphics.no>
2024-11-07 18:40:20 +0100

    beepboop

---
+++ websites/experiments/PhotoGalleryExperiment/galleryScript.js
@@ -0,0 +1,22 @@
+let imageArray = [];
+
+
+
+let newImage = new Image();
+
+
+// img list
+newImage.src = '/img/Gallery/3D/1001.png';
+newImage.src = '/img/Gallery/3D/IMG_0525.jpg';
+newImage.src = '/img/Gallery/3D/IMG_0526.jpg';
+newImage.src = '/img/Gallery/3D/IMG_0528.jpg';
+// newImage.src('/img/Gallery/3D/ ');
+
+
+
+// img pushing
+imageArray.push(newImage);
+
+let displayArray = document.querySelector('imageGalleryElement').innerHTML;
+
+displayArray;
