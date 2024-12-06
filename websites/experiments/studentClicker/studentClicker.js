730febd431552ca49a5b954316c89fa578044b61
OG-Design <marcus@olsen-graphics.no>
2024-12-06 12:50:40 +0100

    2-06/12/24 Student Clicker project
    some functionality

--- websites/experiments/studentClicker/studentClicker.js
+++ websites/experiments/studentClicker/studentClicker.js
@@ -1,18 +1,50 @@
+
+// score value
 let score = 0;
 
+// score encrease value
+let scoreIncrementValue = 1;
 
-// button event and function --START
-document.getElementById("html_buttonClick").addEventListener("click", buttonClick);
 
-function buttonClick() {
-    console.log("debug-function buttonClick");
+
+
+// student_Button event and function --START
+document.getElementById("html_buttonClick").addEventListener("click", student_ButtonClickJs);
+
+// score display
+let scoreDisplayJs = document.getElementById("html_scoreDisplayValue");
+
+function student_ButtonClickJs() {
+    console.log("debug-function student_buttonClick");
     
     // incremental increase
-    score ++;
+    // score ++;
+    // custom incremental increase
+    score = score + scoreIncrementValue;
+
     
+
     // check score
     console.log("Unemployed: ", score);
 
+    // write score to html.
+    scoreDisplayJs.innerText = score;
 
 }
-// button event and function --END
+// student_Button event and function --END
+
+
+
+// headerMenu --START
+let headerMenu_EducationalFacilitiesButtonClick = document.getElementById("html_menuButton_EducationalFacilities").addEventListener("click", headerMenu_EducationalButtonClickFacilitiesJsFunction);
+let headerMenu_EducationalFacilities_menuFrameJs = document.getElementById("displayMenu_EducationalFacilities").style.display = "none";
+
+function headerMenu_EducationalButtonClickFacilitiesJsFunction() {
+    // debug
+    console.log("debug-function headerMenu_EducationalFacilitiesButtonClick");
+    
+    headerMenu_EducationalFacilities_menuFrameJs.style.display ="";
+
+
+}
+// headerMenu --END
