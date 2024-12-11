2e383eb8255151244035442312bc699c1f77d8ee
OG-Design <marcus@olsen-graphics.no>
2024-12-11 20:29:12 +0100

    img gallery improovements js

--- websites/experiments/klokkeBakgrunn/script.js
+++ websites/experiments/klokkeBakgrunn/script.js
@@ -6,10 +6,7 @@ imgIdReadable="image ";
 // number value
 imgIdValue=0;
 
-
-
-
-function imageFunction(src, width, height, alt) {
+function imageFunction(src, height, alt) {
     
     
 
@@ -19,13 +16,13 @@ function imageFunction(src, width, height, alt) {
 
     
     img.src = src;
-    img.width = width;
+    // img.width = width;
     img.height = height;
     img.alt = alt;
     
     
 
-    // if id is equal to or smaller increase id+1
+    // if id is equal to or smaller increase id++
     if (imgIdValue<=imgIdValue) {
         imgIdValue++;
     }
@@ -33,31 +30,73 @@ function imageFunction(src, width, height, alt) {
     // put together id
     imgId = imgIdReadable+imgIdValue;
     
-    // Debug
-    console.log(imgIdValue);
+    // debug
+    // console.log(imgIdValue);
+    // console.log("functionTest imageFunction");
     
-    
-    
-    console.log("functionTest");
-    
+
+
     // finds class .imageGallery and appends child img to it, aswell as seting an understandable id that increases per img.
     document.querySelector(".imageGallery").appendChild(img).setAttribute("id", imgId);
-    // document.getElementById(imgId).style.visibility="hidden";
+
+    // hides images
+    document.getElementById(imgId).style.display="none";
+    
+    
 }
 
 // let bilde = document.createElement("img");
 // bilde.src = "img/spareGris.svg";
 // document.querySelector(".imageGallery").appendChild();
 
-// images
-let img1_js = imageFunction("img/spareGris.svg", 1000, 1000, "piggyBank svg");
-let img2_js = imageFunction("img/spareGris.svg", 1000, 1000, "piggyBank svg");
-
-console.log("test1");
-
-let imageArray = [img1_js];
 
 
+// images to diaplay
+let img1_js = imageFunction("img/spareGris.svg", 350, "piggyBank svg");
+let img2_js = imageFunction("/img/Gallery/3D/1001.png", 350, "piggyBank svg");
+let img3_js = imageFunction("/img/gallery/3D/IMG_0525.jpg", 350, "piggyBank svg");
+let img4_js = imageFunction("/img/gallery/3D/IMG_0528.jpg", 350, "piggyBank svg");
+
+// debug
+// console.log("test1");
+// console.log("\n\n\n\n");
+
+let imageArray = [img1_js, img2_js, img3_js, img4_js];
+
+
+
+
+
+// idRead reads id's and displays their parent.
+function idRead(){
+    console.log("img  id:","\n\n");
+    // adds for later subtraction, avoids id error...
+    imgIdValue++;
+
+    // forLoop for idRead
+    for (let i = 0; i<imageArray.length;i++) {
+    
+        // subtracts imgIdValue for each loop 
+        imgIdValue=imgIdValue-1;
+    
+        // redefines imgId in for loop
+        imgId = imgIdReadable + imgIdValue;
+    
+        // displays array elements with id
+        document.getElementById(imgId).style.display="block";
+    
+        // checkValue
+        // console.log(imgIdValue);
+        
+        // checkWholeId
+        console.log(imgId);
+
+    }
+    
+
+}
+// runs function idRead
+idRead();
 
 
 
