15f943975e7aa64eaaeab3ce5bfb809038fbdcdd
peremarai <marcus@olsen-graphics.no>
2024-12-18 09:01:04 +0100

    12/18/24 ExperimentalHomePage: la til funksjonalitet
    La til funksjonalitet til dotMenu. lagde enkel nav

--- websites/experiments/HomePageDesign1/js/navScript.js
+++ websites/experiments/HomePageDesign1/js/navScript.js
@@ -1,5 +1,6 @@
 // defines nav
-let headerNav_js = document.querySelector(".headerNav_html");
+let headerNav_logoMenu_js = document.querySelector(".headerNav_logoMenu_html");
+let headerNav_dotMenu_js = document.querySelector(".headerNav_dotMenu_html");
 
 // defines headerLogo
 let headerLogo_js = document.querySelector(".headerLogo");
@@ -7,31 +8,35 @@ let headerLogo_js = document.querySelector(".headerLogo");
 
 
 // hides header nav
-headerNav_js.style.display="none";
+headerNav_logoMenu_js.style.display="none";
+headerNav_dotMenu_js.style.display="none";
 
 // listens for click and executes navMenuFunction on click
-document.querySelector(".logoButton_html").addEventListener("click", navMenuFunction);
+document.querySelector(".logoButton_html").addEventListener("click", headerNav_logoMenuFunction);
+document.querySelector(".dotMenuButton_html").addEventListener("click", headerNav_dotMenu_jsFunction);
+
 
 // listens for click event temp fix 
-document.querySelector(".mainBody").addEventListener("click", generalClickFunction)
+document.querySelector(".mainBody").addEventListener("click", generalClickFunction);
 
 
 // if main in body clicked then disable menu visibility
 function generalClickFunction() {
-    if (headerNav_js.style.display=="block") {
-        headerNav_js.style.display="none";
+    if (headerNav_logoMenu_js||headerNav_dotMenu_js.style.display=="block") {
+        headerNav_logoMenu_js.style.display="none";
+        headerNav_dotMenu_js.style.display="none";
     }        
 }
 
 
-// event function navMenu
-function navMenuFunction() {
+// event function headerNav_logoMenuFunction
+function headerNav_logoMenuFunction() {
 
 
     // double equals for compare
-    if (headerNav_js.style.display=="none") {
+    if (headerNav_logoMenu_js.style.display=="none") {
         // shows nav
-        headerNav_js.style.display="block";
+        headerNav_logoMenu_js.style.display="block";
 
         
         // removes headerLogo animation
@@ -42,10 +47,38 @@ function navMenuFunction() {
     } 
     else {
         // hides nav
-        headerNav_js.style.display="none";
+        headerNav_logoMenu_js.style.display="none";
 
         // adds headerLogo animation
-        // headerLogo_js.classList.add("headerLogo_animation_css");  
+        // headerNav_logoMenuFunction.classList.add("headerLogo_animation_css");  
+
+        // debug
+        console.log("invisible: headerNav ");
+    }
+}
+
+// event function headerNav_logoMenuFunction
+function headerNav_dotMenu_jsFunction() {
+
+
+    // double equals for compare
+    if (headerNav_dotMenu_js.style.display=="none") {
+        // shows nav
+        headerNav_dotMenu_js.style.display="block";
+
+        
+        // removes headerLogo animation
+        headerNav_dotMenu_js.classList.remove("headerLogo_animation_css");        
+
+        // debug
+        console.log("visible: headerNav ");
+    } 
+    else {
+        // hides nav
+        headerNav_dotMenu_js.style.display="none";
+
+        // adds headerLogo animation
+        // headerNav_dotMenu_js.classList.add("headerLogo_animation_css");  
 
         // debug
         console.log("invisible: headerNav ");
