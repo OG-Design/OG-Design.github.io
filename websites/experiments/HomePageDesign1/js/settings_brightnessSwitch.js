288e2b2f1af716c402637d87744a235d75088ebc
OG-Design <marcus@olsen-graphics.no>
2024-12-19 16:03:09 +0100

    OG eksperimental page: farge endring funker
    fikk fikset farge slik at den endrer seg på den måten jeg vil. Function er kommentert ut siden jeg ikke er ferdig

--- websites/experiments/HomePageDesign1/js/settings_brightnessSwitch.js
+++ websites/experiments/HomePageDesign1/js/settings_brightnessSwitch.js
@@ -1,20 +1,37 @@
 
 function baseCol_1_bg() {
     let baseCol_1_bg_jsAll = document.querySelectorAll(".baseCol_1_bg");
-    
+    let baseCol_1_bgDark_jsAll = document.querySelectorAll(".baseCol_1_bgDark");
     idBaseCol1_bg="baseCol_bg_";
     
 
-    if ( baseCol_1_bg_jsAll != null) {
+    if ( baseCol_1_bg_jsAll != null ) {
 
-        for(i=0 ; i < baseCol_1_bg_jsAll.length; i++ ){
+        // for loop to assign id to each element, aswell as giving it a different class.
+        for( i = 0 ; i < baseCol_1_bg_jsAll.length; i++ ) {
         
+            // defines baseCol_1_bg id, adds i to id to give different id to each element/run.
             let idMake = baseCol_1_bg_jsAll[i].id=idBaseCol1_bg+ i;
             
-            document.getElementById(idMake).setAttribute("class"(".baseCol_1_bg"));
-            document.getElementById(idMake).removeAttribute("class",".baseCol_1_bg");
+            document.getElementById(idMake).setAttribute("class","baseCol_1_bgDark");
+            document.getElementById(idMake).classList.remove(".baseCol_1_bg");
+            
+        }
 
-        };
+
+    } else if ( baseCol_1_bgDark_jsAll != null ) {
+
+
+        // for loop to assign id to each element, aswell as giving it a different class.
+        for( i = 0 ; i < baseCol_1_bg_jsAll.length; i++ ) {
+        
+            // defines baseCol_1_bg id, adds i to id to give different id to each element/run.
+            let idMake = baseCol_1_bg_jsAll[i].id=idBaseCol1_bg+ i;
+            
+            document.getElementById(idMake).setAttribute("class","baseCol_1_bgDark");
+            document.getElementById(idMake).classList.remove(".baseCol_1_bg");
+            
+        }
 
 
     }
