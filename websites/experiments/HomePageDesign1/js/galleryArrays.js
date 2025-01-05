dffefddf9829460ac44bd031413bc484d161019c
peremarai <marcus@olsen-graphics.no>
2025-01-05 13:48:28 +0100

    OG experimental page: start of image array preset in js

---
+++ websites/experiments/HomePageDesign1/js/galleryArrays.js
@@ -0,0 +1,10 @@
+let pathToImg_3d = document.querySelector("#galleyContentParent_3d");
+
+let imgParentCreate = document.createElement("div").id="";
+
+let imgParentAppend = pathToImg_3d.appendChild("div"); 
+
+
+
+let ImageArray_3d = [];
+
